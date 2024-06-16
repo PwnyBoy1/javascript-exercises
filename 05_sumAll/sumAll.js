64a48abcaf8689954fa67dd3272fd654dbc7ca86
pwnyboy1 <fredi.rivera@hotmail.com>
2024-06-15 23:16:46 -0400

    Complete Exersises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    arr = [];
+    sum = 0;
+    if (!Number.isInteger(min) ||!Number.isInteger(max) ) return "ERROR";
+    if( min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+      }
+    for (let i = min; max >= i; i++){
+        arr.push(i);
+    }
+    arr.forEach( x =>{sum += x} );
+return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = ((temp - 32) * (5/9))
+    round = Math.round(temp*10)/10;
+    return round;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(temp) {
+    temp = (temp * (9/5)+32)
+    round = Math.round(temp*10)/10;
+    return round;
+    
 };
 
 // Do not edit below this line
